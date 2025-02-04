1f0eadc432ad8599a096090c4375cf114467efc4
Pratik1603 <pratikgupta1603@gmail.com>
2025-02-04 20:55:23 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -84,5 +84,5 @@ app.get('*',(req,res)=>{
 })
 
 // ✅ Start Server
-const PORT = process.env.PORT || 5001;
+const PORT = 5001;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
