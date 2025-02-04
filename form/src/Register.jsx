083a13931f0315a6862ebbe49fa0894360a04f7a
Pratik1603 <pratikgupta1603@gmail.com>
2025-02-04 23:51:44 +0530

    update

--- form/src/Register.jsx
+++ form/src/Register.jsx
@@ -24,7 +24,7 @@ function Register() {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await fetch('http://localhost:5001/register', {
+            const response = await fetch('https://yogaapp-x1ez.onrender.com/register', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
